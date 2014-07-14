3c537732ed5c65e21fc867b5bdff3e0c2ab0cb6b
vinsonla@yahoo.com <vinsonla@yahoo.com>
2014-07-13 22:38:06 -0700

    Added World Map class

---
+++ EscapeTheGame/WorldMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EscapeTheGame
+{
+    public class WorldMap
+    {
+        private List<PictureBox> tiles;
+
+        public WorldMap(int width, int height, Form form)
+        {
+
+        }
+    }
+}
