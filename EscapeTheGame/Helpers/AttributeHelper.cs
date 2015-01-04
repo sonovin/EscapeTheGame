976fcfdcd8d0aca25155d3382bc7de8cdb787cff
vinsonla@yahoo.com <vinsonla@yahoo.com>
2015-01-03 23:11:59 -0800

    Added Helper class to add attributes duration

---
+++ EscapeTheGame/Helpers/AttributeHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscapeTheGame.Helpers
+{
+    public class AttributeHelper
+    {
+        public List<Attributes> UpdateStatus(List<Attributes> statusList, Attributes.Attribute targetStatus, int duration)
+        {
+            var found = false;
+            for (int i = 0; i < statusList.Count; i++)
+            {
+                var status = statusList[i];
+                if (status.Effect == targetStatus)
+                {
+                    status.Duration += duration;
+                    statusList[i] = status;
+                    found = true;
+                    break;
+                }
+            }
+            if (!found)
+            {
+                statusList.Add(new Attributes(targetStatus,duration));
+            }
+            return statusList;
+        }
+    }
+}
